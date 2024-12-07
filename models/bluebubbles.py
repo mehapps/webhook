93fab42627cac0fcfe66dfadd599c8573ece221b
mehul <github@mehapps.com>
2024-12-07 15:07:18 -0500

    Update BlueBubbles model

--- models/bluebubbles.py
+++ models/bluebubbles.py
@@ -6,9 +6,15 @@ class Handle(BaseModel):
     service: str
     uncanonicalizedId: str
     country: str
-    class Config:
-        extra = 'allow'
-    
+
+class Chats(BaseModel):
+    originalROWID: int
+    guid: str
+    style: int
+    chatIdentifier: str
+    isArchived: bool
+    displayName: str | None = None
+
 class Data(BaseModel):
     guid: str
     text: str | None = None
@@ -16,11 +22,29 @@ class Data(BaseModel):
     dateEdited: int | None = None
     isFromMe: bool
     handle: Handle
-    class Config:
-        extra = 'allow'
-
-class BluebubblesData(BaseModel):
-    type: str
-    data: Data
-    class Config:
-        extra = 'allow'
+    originalROWID: int
+    attributedBody: str | None = None
+    handleId: int
+    otherHandle: int
+    attachments: list | None = None
+    subject: str | None = None
+    error: int
+    dateRead: int | None = None
+    dateDelivered: int | None = None
+    isDelivered: bool
+    hasDdResults: bool
+    isArchived: bool
+    itemType: int
+    groupTitle: str | None = None
+    groupActionType: int
+    balloonBundleId: str | None = None
+    associatedMessageGuid: str | None = None
+    associatedMessageType: int | None = None
+    expressiveSendStyleId: str | None = None
+    threadOriginatorGuid: str | None = None
+    hasPayloadData: bool
+    chats: list[Chats] | None = None
+    messageSummaryInfo: dict | None = None
+    payloadData: dict | None = None
+    dateRetracted: int | None = None
+    partCount: int
