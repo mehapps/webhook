3ef8eb675fce2f73d4b5cfdb949f1235080e7461
mehul <github@mehapps.com>
2024-12-08 19:19:43 -0500

    switch to refresh

--- app/main.py
+++ app/main.py
@@ -332,7 +332,7 @@ async def location_request(handle: str):
                 "last_updated": past_location["last_updated"]
                 }
     
-    url = f"{BB_ADDRESS}/api/v1/icloud/findmy/friends?password={BB_PASSWORD}"
+    url = f"{BB_ADDRESS}/api/v1/icloud/findmy/friends/refresh?password={BB_PASSWORD}"
     request = requests_get(url)
     json_data = request.json()
 
